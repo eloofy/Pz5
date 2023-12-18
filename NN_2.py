9c320c981700803d44d8b2202cb5477c258882b2
anatoly <t.vlasov23@mail.ru>
2023-12-18 16:15:25 +0300

    Initial commit

---
+++ NN_2.py
@@ -0,0 +1,85 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
+
+class SimpleNet(nn.Module):
+    def __init__(self):
+        super(SimpleNet, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+
+            nn.Flatten(),
+            nn.Linear(32 * 32 * 32, 10)
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+def calculate_metrics(loader, model):
+    y_true = []
+    y_pred = []
+    model.eval()
+    with torch.no_grad():
+        for inputs, labels in loader:
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs, 1)
+            y_pred.extend(predicted.numpy())
+            y_true.extend(labels.numpy())
+
+    accuracy = accuracy_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred, average='macro')
+    precision = precision_score(y_true, y_pred, average='macro')
+    f1 = f1_score(y_true, y_pred, average='macro')
+    return accuracy, recall, precision, f1
+
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+     ]
+)
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+valset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
+valloader = DataLoader(valset, batch_size=64, shuffle=False)
+
+net = SimpleNet()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
+
+for epoch in range(2):
+    running_loss = 0.0
+    net.train()
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+
+        optimizer.zero_grad()
+
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    print(f'Epoch {epoch + 1}, Loss: {running_loss / len(trainloader):.3f}')
+
+    train_acc, train_rec, train_prec, train_f1 = calculate_metrics(trainloader, net)
+    print(f'Training - Accuracy: {train_acc}, Recall: {train_rec}, Precision: {train_prec}, F1 Score: {train_f1}')
+
+    val_acc, val_rec, val_prec, val_f1 = calculate_metrics(valloader, net)
+    print(f'Validation - Accuracy: {val_acc}, Recall: {val_rec}, Precision: {val_prec}, F1 Score: {val_f1}')
+
+print('Finished Training')
